5344cc0910e91e4d8de259e9b67827f94a2c7733
Alexander Pann <alexander.pann@gmail.com>
2014-04-02 16:26:47 +0200

    Initial commit

---
+++ ChatServer/src/com/appsrox/messenger/server/Constants.java
@@ -0,0 +1,14 @@
+package com.appsrox.messenger.server;
+
+public interface Constants {
+
+    /**
+     * Google API Console Browser key.
+     */
+	String API_KEY = "AIzaSyBfy-6-RdA1zAfV0q3Kbr693SdNKTfJDaQ";
+
+	public static final String SENDER_EMAIL 	= "senderEmail";
+	public static final String RECEIVER_EMAIL 	= "receiverEmail";	
+	public static final String REG_ID 			= "regId";
+	public static final String MESSAGE 			= "message";
+}
